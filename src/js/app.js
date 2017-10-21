4f9f3503e3b1edd7572e296501226a075de91af7
Boutaleb Zakariae <zakariaeboutaleb@gmail.com>
2017-10-21 13:15:35 +0100

    middle section done with carousels for plates, footer perfected with color and position, everything is done

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
-function menu() {
-  var $toggle = document.getElementById('menu_toggle');
-  var $sideMenu = document.getElementById('side-menu');
-  var $close = document.getElementById('close');
-  var $tl       = new TimelineMax();
-  $toggle.addEventListener('click', function(){
-    classie.toggle($sideMenu, "sideMenuToggle");
-    $tl.staggerFrom('li', .5, {alpha:0, xPercent:"-=50", ease:Back.easeOut},0.2);
-  });
-  $close.addEventListener('click', function(){
-    classie.remove($sideMenu, "sideMenuToggle");
-  });
-}
-menu();
+$(document).ready(function(){
+  $('.image-overlay i').hover(
+    function() {
+      $( this ).attr('class', 'ion-heart')
+    }, function() {
+      $( this ).attr('class', 'ion-ios-heart-outline')
+    }
+  );
+
+
+});
