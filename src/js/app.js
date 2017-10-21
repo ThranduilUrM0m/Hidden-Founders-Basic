081928dc77b1903db2948942b4d6be35463cd002
Boutaleb Zakariae <zakariaeboutaleb@gmail.com>
2017-10-21 11:12:03 +0100

    head complete, no media queries needed, cuz the world isn't that small.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('say no to racism');
+function menu() {
+  var $toggle = document.getElementById('menu_toggle');
+  var $sideMenu = document.getElementById('side-menu');
+  var $close = document.getElementById('close');
+  var $tl       = new TimelineMax();
+  $toggle.addEventListener('click', function(){
+    classie.toggle($sideMenu, "sideMenuToggle");
+    $tl.staggerFrom('li', .5, {alpha:0, xPercent:"-=50", ease:Back.easeOut},0.2);
+  });
+  $close.addEventListener('click', function(){
+    classie.remove($sideMenu, "sideMenuToggle");
+  });
+}
+menu();
